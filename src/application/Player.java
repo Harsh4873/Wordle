3167845ad64ef8c243171abe4bf0f4a16b0e7955
Harsh4873 <43502626+Harsh4873@users.noreply.github.com>
2024-04-29 13:08:43 -0500

    Create Player.java

---
+++ src/application/Player.java
@@ -0,0 +1,21 @@
+package application;
+
+public class Player extends GameBoard
+{
+    private String userName;
+
+    public Player(String userName) 
+    {
+        this.userName = userName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    
+}
